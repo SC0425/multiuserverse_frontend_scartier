e6f6843dc1f679671ecdfadafd672fe73778066b
SC0425 <SCARTIER@email.uscb.edu>
2025-03-18 19:37:13 -0400

    edited AddUser to POST new user to API, implemented navigate back to home page after adding user

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-//comment
+import axios from 'axios'
+import {Link, useNavigate} from 'react-router-dom'
 export default function AddUser() {
+
+    let navigate=useNavigate()
 
     const [user, setUser]=React.useState({
         name:"",
@@ -14,10 +17,18 @@ export default function AddUser() {
         setUser({...user,[e.target.name]:e.target.value})
     }
 
+    const onSubmit=async(e)=>{
+        e.preventDefault()
+        await axios.post("http://localhost:8080/user",user)
+        navigate("/")
+    }
+
     return <div className="custom-container">
         <div className="row">
             <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
                 <h2 className="text-center m-4">Register User</h2>
+
+                <form onSubmit={(e)=>onSubmit(e)}>
                 <div className="mb-3">
                     <label htmlFor="Name" className="form-label">Name</label>
                     <input type={"text"} className="form-control" placeholder="Enter your name" name="name" value={name} onChange={(e)=>onInputChange(e)}/>
@@ -31,7 +42,8 @@ export default function AddUser() {
                     <input type={"text"} className="form-control" placeholder="Enter your email address" name="email" value={email} onChange={(e)=>onInputChange(e)}/>
                 </div>
                 <button type="submit" className="btn btn-outline-primary">Submit</button>
-                <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+                <Link className="btn btn-outline-danger mx-2" to="/">Cancel</Link>
+                </form>
             </div>
         </div>
     </div>
