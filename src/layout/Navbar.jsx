0c7291bc9407ad5743264240c7a73dc60506c070
SC0425 <SCARTIER@email.uscb.edu>
2025-03-19 21:40:25 -0400

    implemented view single user and main link to home page

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">MultiUserverse</a>
+                    <Link className="navbar-brand" to="/">MultiUserverse</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
